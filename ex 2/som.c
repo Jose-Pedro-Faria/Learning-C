6ab7c57c1a97b66a98e3ef2c22887ad80152a5ad
José Pedro Faria <josepedropfaria@gmail.com>
2021-05-15 08:44:04 +0100

    ex2

---
+++ ex 2/som.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x, y;
+    printf("Dá um valor a x: ");
+    scanf("%i", &x);
+    printf("Dá um valor a y: ");
+    scanf("%i", &y);
+    printf("x + y = %i", x+y);
+}
