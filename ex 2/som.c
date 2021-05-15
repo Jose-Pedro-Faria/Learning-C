f9b727df15b4d8dc36cc7d6b091e3f2b44c97d9d
José Pedro Faria <josepedropfaria@gmail.com>
2021-05-15 08:50:43 +0100

    atualização ex2

--- ex 2/som.c
+++ ex 2/som.c
@@ -3,9 +3,9 @@
 int main ()
 {
     int x, y;
-    printf("Dá um valor a x: ");
+    printf("Da um valor a x: ");
     scanf("%i", &x);
-    printf("Dá um valor a y: ");
+    printf("Da um valor a y: ");
     scanf("%i", &y);
     printf("x + y = %i", x+y);
 }
