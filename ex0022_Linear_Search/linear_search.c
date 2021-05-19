0d30779da4293d407641d50ec24ceaeceb8b1b0f
José Pedro Faria <josepedropfaria@gmail.com>
2021-05-19 21:06:03 +0100

    atualização ex22

--- ex0022_Linear_Search/linear_search.c
+++ ex0022_Linear_Search/linear_search.c
@@ -3,11 +3,14 @@
 int main ()
 {
     int num [] = {4, 6, 8, 2, 7, 5, 0};
+    int num1;
+    printf("Insira um numero e confirme se ele esta na base de dados: ");
+    scanf("%i", &num1);
 
     //Vai percorrer o array todo até encontrar o numero 0
     for (int i = 0; i < 7; i++)
     {
-        if (num[i] == 0)
+        if (num[i] == num1)
         {
             printf("Encontrei\n");
             return 0;
